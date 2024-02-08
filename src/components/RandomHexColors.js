9d5db6f4c9d38172bdb7a05d5324246d7dd6261f
alexa-whitney <alexawhitney@gmail.com>
2024-02-08 14:52:24 -0700

    Added console.log statements for testing

--- src/components/RandomHexColors.js
+++ src/components/RandomHexColors.js
@@ -17,29 +17,36 @@ export default function RandomHexColors() {
 		const r = getRandomInt(255).toString(16).padStart(2, '0');
 		const g = getRandomInt(255).toString(16).padStart(2, '0');
 		const b = getRandomInt(255).toString(16).padStart(2, '0');
-		return `#${r}${g}${b}`;
-	}
+		const hexColor = `#${r}${g}${b}`;
+		// console.log(`Generated Hex Color: ${hexColor}`);
+		return hexColor;
+	};
+
 
 	// Get three random hex colors when the component mounts
 	useEffect(() => {
 		const newColors = [getRandomHexColor(), getRandomHexColor(), getRandomHexColor()];
+		// console.log("New Colors Array:", newColors);
 		setColor(newColors);
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	// This function is called when the user clicks the reset button and also generates three new colors
 	const resetColors = () => {
 		const newColors = [getRandomHexColor(), getRandomHexColor(), getRandomHexColor()];
+		console.log("Reset Colors Array:", newColors);
 		setColor(newColors);
 	}
 
 	return (
 		<div className='game-container'>
 			<div className='square-container'>
-				{colors.map((color, index) => (
-					// Use the 'style' attribute to dynamically set the background color
-					<div key={index} className='square' style={{ backgroundColor: color }}></div>
-				))}
+				{colors.map((color, index) => {
+					// console.log(`Style for square ${index}:`, { backgroundColor: color }); // Log the style object for each square
+					return (
+						<div key={index} className='square' style={{ backgroundColor: color }}></div>
+					);
+				})}
 			</div>
 			<p className='hex-color'>#XXXXXX</p> {/* Placeholder for the hex color */}
 			<p className='status-message'>Correct/Incorrect</p> {/* Placeholder for status message */}
